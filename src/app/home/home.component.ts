06d7e8eb5275cb047ab851a940f4173d5b7ed0f7
DiegoTafurSan13 <tafur232@gmail.com>
2020-01-08 13:36:01 -0500

    third commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ModelService } from "../services/model.service";
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  objbruto: any[] = [];
 
-  constructor() { }
+  constructor(private http: ModelService) { }
 
   ngOnInit() {
+    this.http.getCharacter().snapshotChanges().subscribe(
+      items => {
+        for (let i = 0; i < items.length; i++) {
+          this.objbruto[i] = items[i].payload.toJSON();
+        }
+        console.log(this.objbruto);
+      }
+    );
   }
 
 }
